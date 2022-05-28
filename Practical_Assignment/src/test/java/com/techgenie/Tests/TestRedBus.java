80995bf18d6c2385b01b464778e1a9e3361f81e0
gangadhar-dasari91 <gangadhar.dasari91@gmail.com>
2022-05-28 20:56:50 +0530

    Test 6 - RedBus Completed

--- Practical_Assignment/src/test/java/com/techgenie/Tests/TestRedBus.java
+++ Practical_Assignment/src/test/java/com/techgenie/Tests/TestRedBus.java
@@ -1,9 +1,20 @@
 package com.techgenie.Tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.TreeMap;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -59,7 +70,23 @@ public class TestRedBus {
 		System.out.println(redbusSearchResult.getBusDataItems().size());
 //		redbusSearchResult.getBusDataItems();
 		List<WebElement> busDataItems = redbusSearchResult.getBusDataItems();
-//		
+		
+		 //// Blank workbook
+		XSSFWorkbook workbook = new XSSFWorkbook();
+
+		// create Blank Sheet
+		XSSFSheet sheet1 = workbook.createSheet("Sheet1");
+		XSSFSheet sheet2 = workbook.createSheet("Sheet2");
+		
+		// This Data Need to be written Object[]
+		Map<String, Object[]> data1 = new TreeMap<String, Object[]>();
+		data1.put("1", new Object[] { "Travels", "BusType", "DepartTime" ,"DepartLocation","ArrivalTime","ArrivalLocation","JourneyDuration","StartingFare","Seats Left","Windows Left","Rating"});
+		
+		Map<String, Object[]> data2 = new TreeMap<String, Object[]>();
+		data2.put("1", new Object[] { "Travels", "BusType", "DepartTime" ,"DepartLocation","ArrivalTime","ArrivalLocation","JourneyDuration","StartingFare","Seats Left","Windows Left","Rating"});
+		
+		
+		int idnum = 2;
 		for(WebElement e: busDataItems) {
 			String travels = e.findElement(By.cssSelector("div[class*='travels']")).getText();
 			String busType = e.findElement(By.cssSelector("div[class*='bus-type']")).getText();
@@ -71,22 +98,70 @@ public class TestRedBus {
 			String fare =e.findElement(By.cssSelector("div[class*='seat-fare'] span")).getText();
 			String seatsLeft = (!e.findElements(By.cssSelector("div[class*='seat-left']")).isEmpty())? e.findElement(By.cssSelector("div[class*='seat-left']")).getText().replaceAll("\\D",""): "0";
 			String windowLeft =(!e.findElements(By.cssSelector("div[class*='window-left']")).isEmpty())? e.findElement(By.cssSelector("div[class*='window-left']")).getText().replaceAll("\\D",""): "0";
+			String rating =(!e.findElements(By.cssSelector("div[class*='rating-sec']")).isEmpty())? e.findElement(By.cssSelector("div[class*='rating-sec']")).getText(): "0";
+			
+			float ratingVal = Float.parseFloat(rating);
+			
+			if(ratingVal > 4.0) {
+				data1.put(String.valueOf(idnum++), new Object[] { travels, busType, depTime ,depLoc,arrTime,arrLoc,journeyDur,fare,seatsLeft,windowLeft,rating});
+			}else if(ratingVal < 4.0){
+				data2.put(String.valueOf(idnum++), new Object[] { travels, busType, depTime ,depLoc,arrTime,arrLoc,journeyDur,fare,seatsLeft,windowLeft,rating});
+			}
 			
 			
-			
-			
-			
-			System.out.println(travels+","+busType+","+depTime+","+depLoc+","+arrTime+","+arrLoc+","+journeyDur+","+fare+","+seatsLeft+","+windowLeft);
+//			System.out.println(travels+","+busType+","+depTime+","+depLoc+","+arrTime+","+arrLoc+","+journeyDur+","+fare+","+seatsLeft+","+windowLeft+","+rating);
 			
 		}
+		
+		// Iterate Over data and Write to Sheet
+
+		Set<String> keySet1 = data1.keySet();
+		Set<String> keySet2 = data2.keySet();
+
+		// Write to Sheet 1
+		writeDataToCell(keySet1, data1, sheet1);
+
+		// Write to Sheet 2
+		writeDataToCell(keySet2, data2, sheet2);
+		
+		try {
+			// Write the workbook in the fs
+			FileOutputStream out = new FileOutputStream(new File("RedBusData.xlsx"));
+			workbook.write(out);
+			out.close();
+			System.out.println("Write data to Excel Sucessful");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		Thread.sleep(5000);
 		
 	}
 
+	@AfterMethod
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
 //			softAssert.assertAll();
+	}
+	
+	public static void writeDataToCell(Set<String> keyset,Map<String, Object[]> data,XSSFSheet sheet) {
+		int rownum = 0;
+		for(String key: keyset) {
+			Row row = sheet.createRow(rownum++);
+			Object[] objArr = data.get(key);
+			int cellnum =0;
+			
+			for(Object obj : objArr) {
+				Cell cell = row.createCell(cellnum++);
+				if(obj instanceof String) {
+					cell.setCellValue((String)obj);
+				}else if(obj instanceof Integer) {
+					cell.setCellValue((Integer)obj);
+				}
+			}
+			
+		}
 	}
 
 }
